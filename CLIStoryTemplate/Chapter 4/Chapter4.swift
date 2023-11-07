15e03ab501822d4cb40cab23a80e58f7b412639b
Lauren Lindsey <l.katie1231@gmail.com>
2023-11-07 15:44:58 -0500

    Final updated added conditionals and optionals

--- CLIStoryTemplate/Chapter 4/Chapter4.swift
+++ CLIStoryTemplate/Chapter 4/Chapter4.swift
@@ -8,50 +8,75 @@ import Foundation
 
 func nonProfit() {
     let creatingNonprofit = true
-    print ("Sam created a nonprofit after his life altering event.")
-    print("He named the nonprofit BSAM.")
+    
+    print ("Jonathan created a nonprofit after getting shot, experiencing a life altering event.")
+    print("He named the nonprofit BSAM inspired by the young man who shot him that miraculous night.")
     print("It stands for boys sports, art, and music.")
-    print("BSAM focuses on youth development.")
+    print("BSAM is for young males and their social, emotional, and educational development within the community of LA.")
+   
     if creatingNonprofit {
-        print("Every week, the nonprofit focuses on different positive subjects such as ")
+        
+        print("Every week, they focus on different subjects and activities to uplift themselves and each other. ")
     }
     
+    var fieldTrips = 2
+    var jobSearch = 1
     
+    if fieldTrips > jobSearch {
+        print("They go on more fieldtrips for fun to attend")
+    }
+    
+
+   
+
     //func subjectList() {
     //    let subjectList = ["Music", "Art", " and Sports."]
     //    print (subjectList.joined(separator: ", "))
     //    print("BSAMs name originates from Sam, the yong boy who shot Johnathan")
 }
-enum Activities {
-    case music, art, sports
-}
-func Arts(subjects: Activities) {
-    switch subjects {
-    case .art:
-        print("art, music, sports")
-    case .music:
-        print("BSAM")
-    case .sports:
-        print("this is fake")
-    }
-}
-
-    
-//struct character {
-   // let nonProfit:
-   // let subject:String
-    
+//enum Activities {
+//    case music, art, sports
 //}
-//let arr:[subjects] = [.music, .art, .sports]
+//func Arts(subjects: Activities) {
+//    switch subjects {
+//    case .art:
+//        print("art, music, and sports.")
+//    case .music:
+//        print("BSAM")
+//    case .sports:
+//        print("this is fake")
+//    }
+//}
 
-//for objects in arr {
+func samIsAlwaysPresent(place: String) -> Int? {
+    if place == "LA" { return 15 }
+    if place == "Detroit" { return 16 }
+    if place == "BSAM" { return 18 }
+    if place == "Inglewood" { return 17 }
+    if place == "Compton" { return 14 }
+    
+    return nil
+}
 
 func chapterFour() {
-    
+    var age = samIsAlwaysPresent(place: "BSAM")
     nonProfit()
-    print(Arts(subjects: .art))
+//    print(Arts(subjects: .art))
     
-    
-    
+    enum sports {
+        case basketball, football, hockey, games
+    }
+    struct fieldTrips {
+        let extracurriculars: sports
+        let name:String
+    }
+    let arr:[sports] = [.basketball,.football,.hockey, .games]
+for sports in arr {
+            print(sports)
+        }
+    if year == nil {
+        print("There was an error")
+    } else {
+        print("Sam is now \(age!) and is consistantly present at the BSAM office. Now as a thriving mentor who helps facilitate projects and activities for several young boys at the organization, he has become a pivital part of BSAM itself and the larger part of youth culture in the ganglands of LA.")
+    }
 }
-
