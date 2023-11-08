f761e9e7cd6db608507464d4e7b5dbb33ff556c9
Lauren Lindsey <l.katie1231@gmail.com>
2023-11-08 13:34:51 -0500

    Final changes

--- CLIStoryTemplate/Chapter 4/Chapter4.swift
+++ CLIStoryTemplate/Chapter 4/Chapter4.swift
@@ -8,45 +8,27 @@ import Foundation
 
 func nonProfit() {
     let creatingNonprofit = true
+//declared function as non profit
+    
     
     print ("Jonathan created a nonprofit after getting shot, experiencing a life altering event.")
     print("He named the nonprofit BSAM inspired by the young man who shot him that miraculous night.")
     print("It stands for boys sports, art, and music.")
     print("BSAM is for young males and their social, emotional, and educational development within the community of LA.")
-   
+    
     if creatingNonprofit {
         
         print("Every week, they focus on different subjects and activities to uplift themselves and each other. ")
     }
     
-    var fieldTrips = 2
-    var jobSearch = 1
+    let fieldTrips = 2
+    let jobSearch = 1
     
     if fieldTrips > jobSearch {
         print("They go on more fieldtrips for fun to attend")
     }
-    
-
-   
-
-    //func subjectList() {
-    //    let subjectList = ["Music", "Art", " and Sports."]
-    //    print (subjectList.joined(separator: ", "))
-    //    print("BSAMs name originates from Sam, the yong boy who shot Johnathan")
 }
-//enum Activities {
-//    case music, art, sports
-//}
-//func Arts(subjects: Activities) {
-//    switch subjects {
-//    case .art:
-//        print("art, music, and sports.")
-//    case .music:
-//        print("BSAM")
-//    case .sports:
-//        print("this is fake")
-//    }
-//}
+
 
 func samIsAlwaysPresent(place: String) -> Int? {
     if place == "LA" { return 15 }
@@ -57,26 +39,45 @@ func samIsAlwaysPresent(place: String) -> Int? {
     
     return nil
 }
+// Used Optional to present value of 18 (Sam's Age) if input == BSAM you will get the output 18
 
 func chapterFour() {
-    var age = samIsAlwaysPresent(place: "BSAM")
     nonProfit()
-//    print(Arts(subjects: .art))
     
     enum sports {
-        case basketball, football, hockey, games
+        case basketball, football, games
     }
+    //created enum and named it sports, with different sports categories as the cases
+    
     struct fieldTrips {
         let extracurriculars: sports
         let name:String
+        
+        
     }
-    let arr:[sports] = [.basketball,.football,.hockey, .games]
-for sports in arr {
-            print(sports)
-        }
-    if year == nil {
+    
+    var myFieldTrip = fieldTrips(extracurriculars: .basketball, name: "Lakers")
+    var yourFieldTrip = fieldTrips(extracurriculars: .football, name: "Lions")
+    print(myFieldTrip.name, myFieldTrip.extracurriculars)
+    print(yourFieldTrip.name, yourFieldTrip.extracurriculars)
+    
+    // inside my enum used variables to name the different kinds of basketball and football games young boys attended in the story
+    
+    let arr:[sports] = [ .games]
+    for sports in arr {
+        print(sports)
+        
+    //array
+    }
+    let age = samIsAlwaysPresent(place: "BSAM")
+    //
+    
+    
+    
+    if age == nil {
         print("There was an error")
     } else {
         print("Sam is now \(age!) and is consistantly present at the BSAM office. Now as a thriving mentor who helps facilitate projects and activities for several young boys at the organization, he has become a pivital part of BSAM itself and the larger part of youth culture in the ganglands of LA.")
+        // used conditional (if, else statement to print last sentence if sam's age is = nil or something other than  18 it wont print but if his age = 18 it will.
     }
 }
